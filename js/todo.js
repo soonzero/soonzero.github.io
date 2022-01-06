54d3e43eb1494c690242e75135405f8ad5ce45ef
soonzero <95613159+soonzero@users.noreply.github.com>
2022-01-06 23:08:54 +0900

    Add files via upload

---
+++ js/todo.js
@@ -0,0 +1,53 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+const TODOS_KEY = "todos"
+
+let toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const chosenToDo = event.target.parentElement;
+    chosenToDo.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(chosenToDo.id));
+    saveToDos();
+}
+
+function paintToDo(newToDo) {
+    const li = document.createElement("li")
+    li.id = newToDo.id;
+    const span = document.createElement("span");
+    span.innerText = newToDo.text;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "❌";
+    deleteBtn.addEventListener("click", deleteToDo);
+    li.appendChild(deleteBtn);
+    li.appendChild(span);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = "";
+    const newToDoObj = {
+        text: newToDo, 
+        id: Date.now()
+    }
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
+    saveToDos();
+}
+
+toDoForm .addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
