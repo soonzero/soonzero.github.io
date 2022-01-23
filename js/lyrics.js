cf12c50ab8da5a02efb519bf9881d7168dd3349b
soonzero <sykeum0912@naver.com>
2022-01-23 16:58:49 +0900

    lyrics changed

--- js/lyrics.js
+++ js/lyrics.js
@@ -1,43 +1,43 @@
 const lyricsArray = [
   {
-    lyrics: "몹시도 사랑히 적어둔 글씨들에",
-    title: "러브 레터",
+    lyrics: "I love myself, I trust myself",
+    title: "내게 들려주고 싶은 말(Dear Me)",
   },
   {
-    lyrics: "봄 몇 송이, 여름 한 컵, 가을 한 장, 그리고 겨울 한 숨",
+    lyrics: "수고했어요, 정말 고생했어요. 그댄 나의 자랑이죠",
+    title: "하루의 끝(End of a day)",
+  },
+  {
+    lyrics: "잠깐 쉬어도 좋아 쉬엄쉬엄도 좋아 무리하지 않아도 돼",
+    title: "Marathon",
+  },
+  {
+    lyrics: "네 모습 그대로 그래 괜찮아 괜찮아도",
+    title: "괜찮아도 괜찮아(That's okay)",
+  },
+  {
+    lyrics: "우린 빛나고 있네, 각자의 방 각자의 별에서",
+    title: "소우주(Microkosmos)",
+  },
+  {
+    lyrics: "가끔은 실수 해도 돼, 누구든 그랬으니까",
+    title: "한숨",
+  },
+  {
+    lyrics: "봄 몇 송이, 여름 한 컵, 가을 한 장, 겨울 한 숨",
     title: "겨울잠",
   },
   {
-    lyrics: "이 밤 그날의 반딧불을 당신의 창 가까이 보낼게요",
-    title: "밤편지",
+    lyrics: "넌 혼자가 아니야 네 곁엔 내가 있잖아",
+    title: "Smile Again",
   },
   {
-    lyrics: "감히 이 마음만은 주름도 없이 여기 반짝 살아있어요",
-    title: "마음",
+    lyrics: "나의 길을 가고 있다고 외치면 돼",
+    title: "나를 외치다",
   },
   {
-    lyrics: "아득히 떨어진 곳에서, 끝없이 흐노는 누구를 알까",
-    title: "너",
-  },
-  {
-    lyrics: "바람을 세로질러 날아오르는 기분 so cool",
-    title: "strawberry moon",
-  },
-  {
-    lyrics: "네 말대로 언젠가 나도 나 같은 누군가에게 사랑 받게 될까",
-    title: "이런 엔딩",
-  },
-  {
-    lyrics: "백만송이장미꽃을, 나랑피워볼래?",
-    title: "Blueming",
-  },
-  {
-    lyrics: "황혼을 따라 춤추는 그늘 길어지는데",
-    title: "정거장",
-  },
-  {
-    lyrics: "I got this, I've truly found 이제 조금 알 것 같아 날",
-    title: "Palette",
+    lyrics: "Are we all lost stars, trying to light up the dark?",
+    title: "Lost Stars",
   },
 ];
 
